3c472b7ab0833a778f57d8731b7f81278bf57595
Song Khoi <songkhoi123@gmail.com>
2025-02-27 00:44:43 +0700

    using partial

--- app/Main.hs
+++ app/Main.hs
@@ -4,6 +4,10 @@ main :: IO ()
 main = putStrLn myhtml
   where
     myhtml = makeHtml "Hello title" "Hello, world!"
+    makeHtml :: String -> (String -> String)
+    el :: String -> String -> String
+    el tag content =
+      "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
     makeHtml title content =
       html_
         ( head_
@@ -13,11 +17,24 @@ main = putStrLn myhtml
             <> body_
               content
         )
-    html_ content =
-      "<html>" <> content <> "</html>"
+    html_ :: String -> String
+    {-
+        html_ content =
+          "<html>" <> content <> "</html>"
+    -}
+    html_ = el "html"
+
+    head_ :: String -> String
     head_ content =
       "<head>" <> content <> "</head>"
+
+    body_ :: String -> String
+    {-
     body_ content =
       "<body>" <> content <> "</body>"
+    -}
+    body_ = el "body"
+
+    title_ :: String -> String
     title_ content =
       "<title>" <> content <> "</title>"
